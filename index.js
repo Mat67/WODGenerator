f78806594a1d1bba1fd70b42af7f3e3caeceadaa
Mathieu Hemmerle <m.hemmerle@afi-esca.com>
2016-12-27 13:58:35 +0100

    Test d'une route

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var tmp = 0
 console.log(tmp)
+var http = require('http')
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+app.listen(8080);
